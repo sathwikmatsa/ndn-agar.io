782029b9cb458ac0edfb29a5e499bf7c139eb0f8
Sathwik Matsa <sathwikmatsa@gmail.com>
2020-03-16 00:34:19 +0530

    add timer class - @lazyfoo

---
+++ src/timer.hpp
@@ -0,0 +1,29 @@
+class Timer {
+public:
+    //Initializes variables
+    Timer();
+
+    //The various clock actions
+    void start();
+    void stop();
+    void pause();
+    void unpause();
+
+    //Gets the timer's time
+    Uint32 get_ticks();
+
+    //Checks the status of the timer
+    bool is_started();
+    bool is_paused();
+
+private:
+    //The clock time when the timer started
+    Uint32 start_ticks;
+
+    //The ticks stored when the timer was paused
+    Uint32 paused_ticks;
+
+    //The timer status
+    bool paused;
+    bool started;
+};

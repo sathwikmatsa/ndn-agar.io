66b00898bd98ae2fc283946de08fe2ab7e79fbdf
abstractxan <priyansh0506@gmail.com>
2020-06-09 22:18:01 +0530

    Add testcasess for Game Messages [WIP]

--- test/test_game_messages.cpp
+++ test/test_game_messages.cpp
@@ -4,6 +4,8 @@
 #include <cassert>
 
 int main() {
+
+  std::cout << "Running test: NewPlayerMessage\t";
   Stream stream = {true};
   // encode
   NewPlayerMessage enpm;
@@ -26,6 +28,98 @@ int main() {
   assert(enpm.r == dnpm.r);
   assert(enpm.g == dnpm.g);
   assert(enpm.b == dnpm.b);
+  std::cout << "OK" << std::endl;
+
+  /* =================================================== */
+  
+  std::cout << "Running test: GameInfoMessage\t";
+  stream={true};
+  // encode
+  GameInfoMessage egim;
+  int r=125,g=120,b=12;
+  int x=10,y=20;
+  egim.player_index = 9;
+  egim.players = std::vector<std::tuple<std::string, uint8_t, uint8_t, uint8_t>>(2,std::make_tuple("Dhruva",r,g,b));
+  egim.pellets = std::vector<std::tuple<int, int, uint8_t, uint8_t, uint8_t>>(2,std::make_tuple(x,y,r,g,b));
+  egim.viruses = std::vector<std::tuple<int, int>>(2,std::make_tuple(x,y));
+
+  egim.serialize(stream);
+  
+  //decode
+  stream.is_writing = false;
+  GameInfoMessage dgim;
+  dgim.serialize(stream);
+  assert(egim.player_index == dgim.player_index);
+  assert(egim.players == dgim.players);
+  assert(egim.pellets == dgim.pellets);
+  assert(egim.viruses == dgim.viruses);
+  std::cout << "OK" << std::endl;
+
+  /* =================================================== */
+  
+  std::cout << "Running test: AtePelletMessage\t";
+  stream={true};
+  // encode
+  AtePelletMessage eapm;
+  eapm.pellet_id = 9;
+  eapm.serialize(stream);
+  //decode
+  stream.is_writing = false;
+  AtePelletMessage dapm;
+  dapm.serialize(stream);
+  assert(eapm.pellet_id == dapm.pellet_id);
+  std::cout << "OK" << std::endl;
+
+  /* =================================================== */
+  
+  std::cout << "Running test: PelletRelocMessage\t";
+  stream={true};
+  // encode
+  PelletRelocMessage eprm;
+  eprm.pellet_id = 9;
+  eprm.pos_x=10;
+  eprm.pos_y=10;
+  eprm.serialize(stream);
+  //decode
+  stream.is_writing = false;
+  PelletRelocMessage dprm;
+  dprm.serialize(stream);
+  assert(eprm.pellet_id == dprm.pellet_id);
+  assert(eprm.pos_x == dprm.pos_x);
+  assert(eprm.pos_y == dprm.pos_y);
+  std::cout << "OK" << std::endl;
+
+  /* =================================================== */
+  
+  std::cout << "Running test: GameOverMessage\t";
+  stream={true};
+  // encode
+  GameOverMessage egom;
+  egom.rank = 9;
+  eapm.serialize(stream);
+  //decode
+  stream.is_writing = false;
+  GameOverMessage dgom;
+  dgom.serialize(stream);
+  assert(egom.rank == dgom.rank);
+  std::cout << "OK" << std::endl;
+
+  /* =================================================== */
+  
+  std::cout << "Running test: PlayerUpdateMessage\t";
+  stream={true};
+  // encode
+  GameOverMessage epum;
+  epum.rank = 9;
+  eapm.serialize(stream);
+  //decode
+  stream.is_writing = false;
+  GameOverMessage dpum;
+  dpum.serialize(stream);
+  assert(epum.rank == dpum.rank);
+  std::cout << "OK" << std::endl;
+
+  /* =================================================== */
 
   std::cout << "All tests passed!" << std::endl;
   return 0;
